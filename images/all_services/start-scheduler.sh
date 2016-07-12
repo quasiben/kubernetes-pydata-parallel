f810046a6e5be09173124dd038e58ca9eaa8ff4b
Benjamin Zaitlen <quasiben@gmail.com>
2016-07-12 04:33:33 +0000

    update docker file to pull s3/aws/jar thingy

--- images/all_services/start-scheduler.sh
+++ images/all_services/start-scheduler.sh
@@ -9,6 +9,6 @@ fi
 
 python /tmp/register.py
 jupyter notebook --ip='*' --port=8080 --no-browser --notebook-dir /opt/app/ --NotebookApp.base_url=/$APP_ID 1> /tmp/notebook.log 2>&1 &
-start-master.sh -h 0.0.0.0 -p 7077 --webui-port 7000
+start-master.sh -h 0.0.0.0 -p 7077 --webui-port 7000 
 ipcontroller &
 dask-scheduler --port=9000 --http-port=9001 --bokeh-port=9002 --bokeh-whitelist="*"
