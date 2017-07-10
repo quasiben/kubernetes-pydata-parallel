81e5d31308781ad81f8bdb4d6774d7097feea6d4
Benjamin Zaitlen <quasiben@gmail.com>
2017-07-10 18:38:25 +0000

    updates

--- images/all_services/start-worker.sh
+++ images/all_services/start-worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /opt/conda/sbin/start-slave.sh spark://schedulers:7077
-/opt/conda/bin/dask-worker schedulers:9000 --nprocs 2 --nthreads 1
+/opt/conda/bin/dask-worker schedulers:9000 --nprocs 1 --nthreads 2
